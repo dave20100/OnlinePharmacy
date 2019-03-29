98d594f5c4a3e74b4c2190254faa71bcf1d9c05e
Dawid <dave201000@gmail.com>
2019-03-29 21:10:17 +0100

    Page 223

--- OnlinePharmacy/Controllers/ProductController.cs
+++ OnlinePharmacy/Controllers/ProductController.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlinePharmacy.Models;
+using System.Linq;
 
 namespace OnlinePharmacy.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
-
+        public int PageSize = 1;
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1) => View(repository.Products.OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize).Take(PageSize));
     }
 }

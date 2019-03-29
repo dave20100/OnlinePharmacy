16636100e7e10334e89c834860252ab9a6a9276b
Dawid Mikowski <dawid.mikowski@stazysta.comarch.pl>
2019-03-29 10:56:29 +0100

    FakeRepositories

---
+++ OnlinePharmacy/Models/FakeProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Ibuprom", Price = 12},
+            new Product {Name = "Gripex", Price = 10}
+        }.AsQueryable<Product>();
+    }
+}
---
+++ OnlinePharmacy/Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Models
+{
+    public class IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}

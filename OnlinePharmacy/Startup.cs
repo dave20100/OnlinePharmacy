241fd8fba0c7512f74d6659763941adc854f07d5
Dawid Mikowski <dawid.mikowski@stazysta.comarch.pl>
2019-03-29 12:22:38 +0100

    connection to DB not working

--- OnlinePharmacy/Startup.cs
+++ OnlinePharmacy/Startup.cs
@@ -7,16 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using OnlinePharmacy.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnlinePharmacy
 {
     public class Startup
     {
+        public IConfiguration Configuration;
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration["Data:PharmacyProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
         }
 

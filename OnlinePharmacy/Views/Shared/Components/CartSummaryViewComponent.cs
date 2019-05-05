10ebbf12193d2f3fc73f2cc3595c19284cd70e0b
Dawid <dave201000@gmail.com>
2019-05-05 17:12:19 +0200

    I need to do refine cart

---
+++ OnlinePharmacy/Views/Shared/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlinePharmacy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Views.Shared.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}

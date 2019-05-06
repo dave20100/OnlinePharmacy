a0e2fc0e86e4c2e356a749adbda8887a3b43b77f
Dawid <dave201000@gmail.com>
2019-05-06 09:36:47 +0200

    found mistake removing from cart works

--- OnlinePharmacy/Models/SessionCart.cs
+++ OnlinePharmacy/Models/SessionCart.cs
@@ -30,6 +30,12 @@ namespace OnlinePharmacy.Models
             Session.SetJson("Cart", this);
         }
 
+        public override void RemoveLine(Product product)
+        {
+            base.RemoveLine(product);
+            Session.SetJson("Cart", this);
+        }
+
         public override void Clear()
         {
             base.Clear();

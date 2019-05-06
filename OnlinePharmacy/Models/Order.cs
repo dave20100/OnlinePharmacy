e0890762322f72c16671c88ce0135199a6a1b0bd
Dawid <dave201000@gmail.com>
2019-05-06 17:31:46 +0200

    Added checkout site

---
+++ OnlinePharmacy/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlinePharmacy.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter the address")]
+        public string Address { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a city name")]
+        public string City { get; set; }
+        public string Zip { get; set; }
+        
+        
+    }
+}
